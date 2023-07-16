568465af0df11d76cd285969d4f541e83d53b636
cdp055 <116390098+cdp055@users.noreply.github.com>
2023-07-15 22:59:45 -0500

    Add files via upload

---
+++ ape-semana13-cdp055/problema01/src/paquete003/BilleteraPagos.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete003;
+
+import paquete001.Persona;
+import paquete004.PagoAguaPotable;
+import paquete004.PagoLuzElectrica;
+import paquete004.PagoPredial;
+import paquete004.PagoTelefonoConvencional;
+
+/**
+ *
+ * @author reroes
+ */
+public class BilleteraPagos {
+    public Persona persona;
+    public double gastoPagos;
+    public String mes;
+    public PagoAguaPotable aguaCasa;
+    public PagoAguaPotable aguaComercio;
+    public PagoLuzElectrica luzCasa;
+    public PagoLuzElectrica luzComercio;
+    public PagoPredial casa1;
+    public PagoPredial casa2;
+    public PagoTelefonoConvencional telefonoCasa;
+    public PagoTelefonoConvencional telefonoFinca;
+
+    public String generarReprote(){
+        StringBuilder reporte = new StringBuilder();
+        reporte.append("Reporte de pago para ").append(persona.nombre).append("").append(persona.apellido).append("\n");
+        reporte.append("Mes: ").append(mes).append("\n");
+        reporte.append("Gasto total en pagos: ").append(gastoPagos).append("\n\n");
+        reporte.append("Detalles de los pagos:\n");
+        reporte.append("Agua casa: ").append(aguaCasa.calcularPago("residencial")).append("\n");
+        reporte.append("Agua Comercio: ").append(aguaComercio.calcularPago("comercial")).append("\n");
+        reporte.append("Luz Casa: ").append(luzCasa.calcularPago()).append("\n");
+        reporte.append("Luz Comercio: ").append(luzComercio.calcularPago()).append("\n");
+        reporte.append("Predial Casa 1: ").append(casa1.calcularPago()).append("\n");
+        reporte.append("Predial Casa 2: ").append(casa2.calcularPago()).append("\n");
+        reporte.append("Telefono Casa: ").append(telefonoCasa.calcularPago()).append("\n");
+        reporte.append("Telefono Finca: ").append(telefonoFinca.calcularPago()).append("\n");
+
+        return reporte.toString();
+    }
+
+}
